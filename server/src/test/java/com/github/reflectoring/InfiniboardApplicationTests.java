06e2ff8b1f945a98cdc3fa441fa208730c7e0f4b
Matthias Balke <matthias.balke@googlemail.com>
2016-01-28 19:51:24 +0100

    initial spring project
    initial gradle config

---
+++ server/src/test/java/com/github/reflectoring/InfiniboardApplicationTests.java
@@ -0,0 +1,18 @@
+package com.github.reflectoring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = InfiniboardApplication.class)
+@WebAppConfiguration
+public class InfiniboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
