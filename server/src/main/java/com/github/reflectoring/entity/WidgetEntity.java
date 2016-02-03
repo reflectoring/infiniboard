90fef69ed8aec11221b59c7f668d8d4d847c5865
Matthias Balke <Matthias.Balke@googlemail.com>
2016-02-03 01:16:07 +0100

    added widget properties
    added sample data

--- server/src/main/java/com/github/reflectoring/entity/WidgetEntity.java
+++ server/src/main/java/com/github/reflectoring/entity/WidgetEntity.java
@@ -1,17 +1,22 @@
 package com.github.reflectoring.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "WIDGET")
 @Entity
 public class WidgetEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "configuration")
+    private String configuration;
+
 
     public Long getId() {
         return id;
@@ -19,5 +24,21 @@ public class WidgetEntity {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(String configuration) {
+        this.configuration = configuration;
     }
 }
---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO WIDGET
+(NAME, CONFIGURATION)
+VALUES ('Build',
+        '"jobs":{"job":{"name":"Jenkins Job DSL","id":1,"url":"https://jenkins.ci.cloudbees.com/job/plugins/job/job-dsl-plugin"}}');
