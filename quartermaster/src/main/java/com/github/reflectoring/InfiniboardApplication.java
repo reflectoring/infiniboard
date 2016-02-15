8e8aea562f848b9b4f5e72d54cecb73ed268d8f5
pa7r1ck <patrick.terstiege@googlemail.com>
2016-02-15 20:27:52 +0100

    alter application start

--- quartermaster/src/main/java/com/github/reflectoring/InfiniboardApplication.java
+++ quartermaster/src/main/java/com/github/reflectoring/InfiniboardApplication.java
@@ -2,11 +2,12 @@ package com.github.reflectoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class InfiniboardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InfiniboardApplication.class, args);
+        new SpringApplicationBuilder().sources(InfiniboardApplication.class).run(args);
     }
 }
