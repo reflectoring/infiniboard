fb458b0af3642af1abf54b0c3175460cddaebc6f
unknown <balke@de-adn-5k9s9t1.adesso.local>
2016-02-19 20:24:44 +0100

    added starter template for angular2 admin-lte

---
+++ infiniboard-app/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "angular2/core";
+
+@Component({
+  selector: 'infiniboard', 
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
+})
+
+export class AppComponent {
+}
