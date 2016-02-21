a1d63d03ddc55ee2b65ef3bea995650a9967c29c
Matthias Balke <matthias.balke@googlemail.com>
2016-02-21 23:01:03 +0100

    added browser-sync, typings to gulp

--- infiniboard-app/gulpfile.js
+++ infiniboard-app/gulpfile.js
@@ -2,62 +2,77 @@ var gulp = require('gulp'),
   rename = require('gulp-rename'),
   traceur = require('gulp-traceur'),
   webserver = require('gulp-webserver'),
+  browserSync = require('browser-sync').create(),
   del = require('del'),
   bower = require('gulp-bower'),
   usemin = require('gulp-usemin'),
   uglify = require('gulp-uglify'),
   minifyCss = require('gulp-minify-css'),
-  replace = require('gulp-replace');
-
-gulp.task('bower', function () {
-  return bower()
-    .pipe(gulp.dest('build/lib/'))
-});
+  replace = require('gulp-replace'),
+  gulpTypings = require("gulp-typings");
 
 // run init tasks
-gulp.task('default', ['bower', 'dependencies', 'js', 'components_html', 'components_css', 'app_html']);
+gulp.task('default', ['dependencies', 'js', 'components_html', 'components_css', 'app_html']);
 
 // run development task
 gulp.task('dev', ['default', 'watch', 'serve']);
 
-// cleans the output folder
+// ---------------------------------------------------------------------------------------------------------------------
+
+// cleans the build dir
 gulp.task('clean', function () {
   del([
     'build'
   ])
 });
 
-// serve the build dir
-gulp.task('serve', ['default'], function () {
-  gulp.src('build')
-    .pipe(webserver({
-      open: true
-    }));
+// ---------------------------------------------------------------------------------------------------------------------
+
+// bower install
+// copy bower dependencies into build dir
+gulp.task('bower', function () {
+  bower()
+    .pipe(gulp.dest('build/lib/'))
+});
+
+// copy dependencies into build dir
+gulp.task('dependencies', function () {
+  return gulp.src([
+    'node_modules/es6-shim/es6-shim.min.js',
+    'node_modules/systemjs/dist/system-polyfills.js',
+    'node_modules/angular2/bundles/angular2-polyfills.js',
+    'node_modules/systemjs/dist/system.src.js',
+    'node_modules/rxjs/bundles/Rx.js',
+    'node_modules/angular2/bundles/angular2.dev.js'
+  ]).pipe(gulp.dest('build/lib'));
+});
+
+// install typings
+gulp.task("install_typings", function () {
+  return gulp.src("./typings.json")
+    .pipe(gulpTypings());
+});
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: "build"
+    }
+  });
 });
 
 // watch for changes and run the relevant task
-gulp.task('watch', function () {
-  gulp.watch('index.html', ['app_html']);
-  gulp.watch('app/**/*.ts', ['js']);
-  gulp.watch('app/**/*.html', ['components_html']);
-  gulp.watch('app/**/*.css', ['components_css']);
-});
-
-// move dependencies into build dir
-gulp.task('dependencies', function () {
-  return gulp.src([
-      'node_modules/es6-shim/es6-shim.min.js',
-      'node_modules/systemjs/dist/system-polyfills.js',
-      'node_modules/angular2/bundles/angular2-polyfills.js',
-      'node_modules/systemjs/dist/system.src.js',
-      'node_modules/rxjs/bundles/Rx.js',
-      'node_modules/angular2/bundles/angular2.dev.js'
-    ])
-    .pipe(gulp.dest('build/lib'));
+gulp.task('watch', ['default'], function () {
+  gulp.watch('index.html', ['app_html']).on('change', browserSync.reload);
+  gulp.watch('app/**/*.ts', ['js']).on('change', browserSync.reload);
+  gulp.watch('app/**/*.html', ['components_html']).on('change', browserSync.reload);
+  gulp.watch('app/**/*.css', ['components_css']).on('change', browserSync.reload);
 });
 
 gulp.task('app_html', function () {
-  gulp.src('index.html')
+  return gulp.src('index.html')
     .pipe(usemin({
       assetsDir: '',
       css: [minifyCss()
@@ -68,7 +83,6 @@ gulp.task('app_html', function () {
     .pipe(replace('bower_components', 'lib'))
     .pipe(gulp.dest('build'));
 });
-
 
 // transpile & move js
 gulp.task('js', function () {
@@ -92,11 +106,11 @@ gulp.task('js', function () {
 // move component html
 gulp.task('components_html', function () {
   return gulp.src('app/**/*.html')
-    .pipe(gulp.dest('build/app'))
+    .pipe(gulp.dest('build/app'));
 });
 
 // move component css
 gulp.task('components_css', function () {
   return gulp.src('app/**/*.css')
-    .pipe(gulp.dest('build/app'))
+    .pipe(gulp.dest('build/app'));
 });
