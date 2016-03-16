dc5da84b3303bf4321c48a07c3b8157d01464b6e
Stefan Kloe <stefan.kloe@gmail.com>
2016-03-16 22:46:47 +0100

    restructured integration tests

--- quartermaster/src/test/java/com/github/reflectoring/InfiniboardApplicationTests.java
+++ quartermaster/src/test/java/com/github/reflectoring/InfiniboardApplicationTests.java
@@ -1,6 +1,8 @@
 package com.github.reflectoring;
 
+import com.github.reflectoring.infiniboard.test.categories.MongoIntegrationTests;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -9,6 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = InfiniboardApplication.class)
 @WebAppConfiguration
+@Category(MongoIntegrationTests.class)
 public class InfiniboardApplicationTests {
 
 	@Test
