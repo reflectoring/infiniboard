73858457036718497ce795d6125290344d8bd287
Matthias Balke <matthias.balke@gmail.com>
2016-03-31 00:41:48 +0200

    enabled prod mode

--- infiniboard-app/app/main.ts
+++ infiniboard-app/app/main.ts
@@ -1,4 +1,6 @@
-import {bootstrap} from "angular2/platform/browser";
-import {AppComponent} from "./app.component";
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+import {enableProdMode} from 'angular2/core';
 
+enableProdMode();
 bootstrap(AppComponent);
