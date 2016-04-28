cd70a475427a4d4f3147201a8bae7e13f75cd2cc
Matthias Balke <matthias.balke@gmail.com>
2016-04-28 22:07:33 +0200

    removed active flag as it is not working dynamic

--- infiniboard-app/app/dashboard/mock-dashboards.ts
+++ infiniboard-app/app/dashboard/mock-dashboards.ts
@@ -2,7 +2,7 @@ import {Dashboard} from './dashboard';
 import {WidgetConfig} from './widget-config';
 
 export var DASHBOARDS: Dashboard[] = [
-  new Dashboard(1, 'Development', 'source metrics', true, [new WidgetConfig('dev', 'platform-status')]),
+  new Dashboard(1, 'Development', 'source metrics', false, [new WidgetConfig('dev', 'platform-status')]),
   new Dashboard(2, 'Support', 'call center metrics', false, [
     new WidgetConfig('harvester', 'jenkins-job'),
     new WidgetConfig('quartermaster', 'jenkins-job'),
