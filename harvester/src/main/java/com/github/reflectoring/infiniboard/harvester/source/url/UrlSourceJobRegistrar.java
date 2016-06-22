cdb11d154e3ae9434a588b0613c9ef729cdae325
Rudi Klassen <rudolfklassen2@gmail.com>
2016-06-22 13:33:30 +0200

    Fixed jobregistration

--- harvester/src/main/java/com/github/reflectoring/infiniboard/harvester/source/url/UrlSourceJobRegistrar.java
+++ harvester/src/main/java/com/github/reflectoring/infiniboard/harvester/source/url/UrlSourceJobRegistrar.java
@@ -4,8 +4,6 @@ import com.github.reflectoring.infiniboard.harvester.scheduling.SchedulingServic
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 /**
  * registers the UrlSourceJob at the SchedulingService
  */
@@ -18,11 +16,6 @@ public class UrlSourceJobRegistrar {
     public UrlSourceJobRegistrar(SchedulingService schedulingService) {
         this.schedulingService = schedulingService;
         this.schedulingService.registerJob(UrlSourceJob.JOBTYPE, UrlSourceJob.class);
-    }
-
-    @PostConstruct
-    public void register() {
-        schedulingService.registerJob(UrlSourceJob.JOBTYPE, UrlSourceJob.class);
     }
 
 }
