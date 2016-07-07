18cf9509e65483a21069f24e4fbd479f6f00ea6d
Matthias Balke <matthias.balke@googlemail.com>
2016-07-07 23:00:08 +0200

    cleanup
    removed mock data
    added widget title
    fixed import

--- infiniboard-app/app/widget/widget.service.ts
+++ infiniboard-app/app/widget/widget.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from 'angular2/core';
 import {Headers, Http, Response} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';
-
+import '../rxjs-operators';
 
 @Injectable()
 export class WidgetService {
