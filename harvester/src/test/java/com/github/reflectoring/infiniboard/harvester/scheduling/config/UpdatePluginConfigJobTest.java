c1eab7b44163c1a4e085b888e7787ccf9c7e9784
Stefan Kloe <stefan.kloe@gmail.com>
2016-07-27 15:09:08 +0200

    Update config timing (#89)
    * changing update source job timing
    
    * reworked UpdateSourceConfigJob

--- harvester/src/test/java/com/github/reflectoring/infiniboard/harvester/scheduling/config/UpdatePluginConfigJobTest.java
+++ harvester/src/test/java/com/github/reflectoring/infiniboard/harvester/scheduling/config/UpdatePluginConfigJobTest.java
@@ -1,14 +1,17 @@
-package com.github.reflectoring.infiniboard.harvester.source.config;
+package com.github.reflectoring.infiniboard.harvester.scheduling.config;
 
 import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Date;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Before;
 import org.junit.Test;
-import org.quartz.JobKey;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.quartz.SchedulerException;
 import org.springframework.context.ApplicationContext;
 
@@ -17,6 +20,7 @@ import com.github.reflectoring.infiniboard.packrat.source.SourceConfig;
 import com.github.reflectoring.infiniboard.packrat.widget.WidgetConfig;
 import com.github.reflectoring.infiniboard.packrat.widget.WidgetConfigRepository;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
 public class UpdatePluginConfigJobTest {
@@ -27,28 +31,49 @@ public class UpdatePluginConfigJobTest {
 
     private WidgetConfigRepository repository;
 
+    private JobExecutionContext jobExecutionContext;
+
+    private final Date fireTime = new Date();
+
     @Before
     public void initializeMocks() {
+        jobExecutionContext = mock(JobExecutionContext.class);
+        when(jobExecutionContext.getFireTime()).thenReturn(fireTime);
+
         applicationContext = mock(ApplicationContext.class);
+        when(jobExecutionContext.getMergedJobDataMap()).thenReturn(
+                new JobDataMap(Collections.singletonMap(SchedulingService.PARAM_CONTEXT, applicationContext)));
+
         schedulingService = mock(SchedulingService.class);
-        repository = mock(WidgetConfigRepository.class);
         when(applicationContext.getBean(SchedulingService.class)).thenReturn(schedulingService);
+
+        repository = mock(WidgetConfigRepository.class);
         when(applicationContext.getBean(WidgetConfigRepository.class)).thenReturn(repository);
     }
 
     @Test
-    public void executeInternalRemembersLastFetchDate() {
+    public void firstExecutionRetrievesAll()
+            throws JobExecutionException {
 
-        UpdatePluginConfigJob job = new UpdatePluginConfigJob();
-        job.executeInternal(applicationContext, new JobKey("update", "harvester"), Collections.emptyMap());
-        job.executeInternal(applicationContext, new JobKey("update", "harvester"), Collections.emptyMap());
-        job.executeInternal(applicationContext, new JobKey("update", "harvester"), Collections.emptyMap());
+        UpdateSourceConfigJob job = new UpdateSourceConfigJob();
+        job.execute(jobExecutionContext);
 
-        // first call searches all configurations
-        verify(repository, atMost(1)).findAll();
-        // subsequent calls search new configurations
-        verify(repository, atLeast(2)).findAllByLastModifiedAfter(any(LocalDateTime.class));
+        verify(repository).findAll();
     }
+
+    @Test
+    public void laterExecutionsRetrieveUpdatedWidgetsOnly()
+            throws JobExecutionException {
+
+        Date previousFireTime = DateUtils.addMinutes(fireTime, -5);
+        when(jobExecutionContext.getPreviousFireTime()).thenReturn(previousFireTime);
+
+        UpdateSourceConfigJob job = new UpdateSourceConfigJob();
+        job.execute(jobExecutionContext);
+
+        verify(repository).findAllByLastModifiedAfter(any(LocalDateTime.class));
+    }
+
 
     @Test
     public void executeInternalSchedulesAllGivenJobs()
@@ -60,8 +85,8 @@ public class UpdatePluginConfigJobTest {
         widgetConfigs.add(createWidgetConfig("firstWidget", "one", "two", "three"));
         widgetConfigs.add(createWidgetConfig("secondWidget", "alpha", "beta", "gamma"));
 
-        UpdatePluginConfigJob job = new UpdatePluginConfigJob();
-        job.executeInternal(applicationContext, new JobKey("update", "harvester"), new HashMap<>());
+        UpdateSourceConfigJob job = new UpdateSourceConfigJob();
+        job.execute(jobExecutionContext);
 
         for (WidgetConfig widget : widgetConfigs) {
             // cleanup old jobs
