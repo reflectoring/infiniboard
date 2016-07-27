20bf76580bf38e4450a1ed70643b2ba1373056c6
Matthias Balke <matthias.balke@googlemail.com>
2016-07-27 22:16:44 +0200

    using same syntax as quartermaster does (#95)
    sonarqube does not like the old one

--- harvester/src/main/java/com/github/reflectoring/infiniboard/harvester/HarvesterApplication.java
+++ harvester/src/main/java/com/github/reflectoring/infiniboard/harvester/HarvesterApplication.java
@@ -1,7 +1,7 @@
 package com.github.reflectoring.infiniboard.harvester;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /**
@@ -19,7 +19,7 @@ public class HarvesterApplication {
      */
     public static void main(String[] args)
             throws Exception {
-        SpringApplication.run(HarvesterApplication.class);
+        new SpringApplicationBuilder().sources(HarvesterApplication.class).run(args);
     }
 
 }
