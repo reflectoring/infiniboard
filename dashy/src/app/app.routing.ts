c574035d16585981c817b82f3f2e8ed3660936ea
Matthias Balke <matthias.balke@gmail.com>
2016-09-07 21:57:38 +0200

    added mocked dashboards route

--- dashy/src/app/app.routing.ts
+++ dashy/src/app/app.routing.ts
@@ -2,9 +2,8 @@ import {Routes, RouterModule} from '@angular/router';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 export const appRoutes: Routes = [
-  {
-    path: '**', component: PageNotFoundComponent
-  }
+  {path: 'dashboards', component: PageNotFoundComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 export const routing = RouterModule.forRoot(appRoutes);
 
