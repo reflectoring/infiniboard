ea2c56348c6b0994d863bbe70f1efe47ccea0dea
Matthias Balke <matthias.balke@gmail.com>
2016-09-09 00:14:38 +0200

    removed old client

--- infiniboard-app/app/main.ts
+++
@@ -1,8 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
-import {enableProdMode, provide} from 'angular2/core';
-import {LocationStrategy, HashLocationStrategy, ROUTER_PROVIDERS} from 'angular2/router';
-
-enableProdMode();
-bootstrap(AppComponent, [ROUTER_PROVIDERS,
-  provide(LocationStrategy, {useClass: HashLocationStrategy})]);
--- infiniboard-app/app/widget/platform-status/status.ts
+++
@@ -1,6 +0,0 @@
-export enum Status {
-  UP,
-  MAINTENANCE,
-  UNKNOWN,
-  DOWN
-}
