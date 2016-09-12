0d46e2db29311cc3066fb432943a7b466060e081
Matthias Balke <matthias.balke@googlemail.com>
2016-09-12 17:11:22 +0200

    added linting to build
    removed forces prefix

--- dashy/src/app/app.component.ts
+++ dashy/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'dashy',
+  selector: 'dashy-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
