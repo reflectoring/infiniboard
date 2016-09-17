4f8008bcce178ae5a65610f446e1a273b919e62e
Matthias Balke <matthias.balke@googlemail.com>
2016-09-17 23:23:12 +0200

    added widget-config tests

--- dashy/src/app/dashboard/shared/widget-config.spec.ts
+++ dashy/src/app/dashboard/shared/widget-config.spec.ts
@@ -1,5 +1,25 @@
+import {WidgetConfig} from './widget-config';
+
 describe('WidgetConfig', () => {
-  // it('should create an instance', () => {
-  //   expect(new WidgetConfig()).toBeTruthy();
-  // });
+  it('should create an instance', () => {
+    expect(new WidgetConfig('platform-status', 'Test Board', '/mock/data')).toBeTruthy();
+  });
+
+  it('constructor() should set widgetConfig.type', () => {
+    let type = 'platform-status';
+    let widgetConfig = new WidgetConfig(type, 'Test Board', '/mock/data');
+    expect(widgetConfig.type).toEqual(type);
+  });
+
+  it('constructor() should set widgetConfig.title', () => {
+    let title = 'Test Board';
+    let widgetConfig = new WidgetConfig('platform-status', title, '/mock/data');
+    expect(widgetConfig.title).toEqual(title);
+  });
+
+  it('constructor() should set widgetConfig.dataLink', () => {
+    let link = '/mock/data';
+    let widgetConfig = new WidgetConfig('platform-status', 'Test Board', link);
+    expect(widgetConfig.dataLink).toEqual(link);
+  });
 });
