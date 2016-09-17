5eb86152a8506263dc90b6ba7a34631370551006
Matthias Balke <matthias.balke@googlemail.com>
2016-09-17 22:05:44 +0200

    added tests for pipes

--- dashy/src/app/dashboard/widget/platform-status-widget/status-color.pipe.spec.ts
+++ dashy/src/app/dashboard/widget/platform-status-widget/status-color.pipe.spec.ts
@@ -1,8 +1,30 @@
 import {StatusColorPipe} from './status-color.pipe';
+import {Status} from './status.enum';
 
 describe('Pipe: StatusColor', () => {
+
   it('create an instance', () => {
     let pipe = new StatusColorPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transforms UP', () => {
+    let pipe = new StatusColorPipe();
+    expect(pipe.transform(Status.UP)).toBe('bg-green');
+  });
+
+  it('transforms MAINTENANCE', () => {
+    let pipe = new StatusColorPipe();
+    expect(pipe.transform(Status.MAINTENANCE)).toBe('bg-yellow');
+  });
+
+  it('transforms UNKNOWN', () => {
+    let pipe = new StatusColorPipe();
+    expect(pipe.transform(Status.UNKNOWN)).toBe('bg-red');
+  });
+
+  it('transforms DOWN', () => {
+    let pipe = new StatusColorPipe();
+    expect(pipe.transform(Status.DOWN)).toBe('bg-red');
   });
 });
--- dashy/src/app/dashboard/widget/platform-status-widget/status-icon.pipe.spec.ts
+++ dashy/src/app/dashboard/widget/platform-status-widget/status-icon.pipe.spec.ts
@@ -1,8 +1,31 @@
 import {StatusIconPipe} from './status-icon.pipe';
+import {Status} from './status.enum';
 
 describe('Pipe: StatusIcon', () => {
+
   it('create an instance', () => {
     let pipe = new StatusIconPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('transforms UP', () => {
+    let pipe = new StatusIconPipe();
+    expect(pipe.transform(Status.UP)).toBe('fa-thumbs-o-up');
+  });
+
+  it('transforms MAINTENANCE', () => {
+    let pipe = new StatusIconPipe();
+    expect(pipe.transform(Status.MAINTENANCE)).toBe('fa-lock');
+  });
+
+  it('transforms UNKNOWN', () => {
+    let pipe = new StatusIconPipe();
+    expect(pipe.transform(Status.UNKNOWN)).toBe('fa-question');
+  });
+
+  it('transforms DOWN', () => {
+    let pipe = new StatusIconPipe();
+    expect(pipe.transform(Status.DOWN)).toBe('fa-remove');
+  });
+
 });
