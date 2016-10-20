d95e7656322663862f372d22245813f7a28253aa
Matthias Balke <matthias.balke@googlemail.com>
2016-10-20 19:30:17 +0200

    Quartermaster API is JSON only (#134)
    * Firefox and IE try to request XML instead of JSON
    
    only JSON is supported
    
    fixes #109 / fixes #110
    
    * only consume JSON

--- quartermaster/src/main/java/com/github/reflectoring/infiniboard/quartermaster/dashboard/rest/DashboardController.java
+++ quartermaster/src/main/java/com/github/reflectoring/infiniboard/quartermaster/dashboard/rest/DashboardController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,7 +35,7 @@ public class DashboardController {
     }
 
     @SuppressWarnings("unchecked")
-    @RequestMapping(method = GET)
+    @RequestMapping(method = GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PagedResources<DashboardResource>> getAllDashboards(@PageableDefault Pageable pageable,
                                                                               PagedResourcesAssembler pagedResourcesAssembler) {
         Page<Dashboard> dashboardsPage = repository.findAll(pageable);
@@ -43,8 +44,7 @@ public class DashboardController {
         return new ResponseEntity<>(pagedResources, OK);
     }
 
-
-    @RequestMapping(value = "/{id}", method = GET)
+    @RequestMapping(value = "/{id}", method = GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<DashboardResource> getDashboard(@PathVariable int id) {
         Dashboard dashboard = repository.find(id);
 
