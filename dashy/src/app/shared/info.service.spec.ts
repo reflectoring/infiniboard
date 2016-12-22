de5e62e02d0cb455c4e76bd292677bdea49aa07a
Matthias Balke <matthias.balke@gmail.com>
2016-12-22 20:18:39 +0100

    injected Http

--- dashy/src/app/shared/info.service.spec.ts
+++ dashy/src/app/shared/info.service.spec.ts
@@ -1,12 +1,22 @@
-/* tslint:disable:no-unused-variable */
+import {TestBed, inject} from '@angular/core/testing';
+import {InfoService} from './info.service';
+import {Info} from './info';
+import {Observable} from 'rxjs';
+import {Http} from '@angular/http';
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { InfoService } from './info.service';
+
+class FakeInfoHttp {
+
+  public getDashboards(): Observable<Info> {
+    return Observable.of(new Info('1.2.3.123'));
+  }
+
+}
 
 describe('Service: Info', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [InfoService]
+      providers: [InfoService, {provide: Http, useValue: FakeInfoHttp}]
     });
   });
 
