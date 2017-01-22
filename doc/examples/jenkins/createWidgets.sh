d26cb025ec377d9ebda3610ef2ce1b20aef424bb
Matthias Balke <matthias.balke@gmail.com>
2017-01-22 18:07:40 +0100

    added example widget configurations
    used public instances although job status may change

--- doc/examples/jenkins/createWidgets.sh
+++ doc/examples/jenkins/createWidgets.sh
@@ -4,5 +4,10 @@ HOST=localhost
 PORT=8080
 ENDPOINT=http://$HOST:8080/api/dashboards/1/widgets
 
-curl -X POST -d @widget.jenkins-folder.json $ENDPOINT --header "Content-Type:application/json"
-curl -X POST -d @widget.jenkins-job.json    $ENDPOINT --header "Content-Type:application/json"
+curl -X POST -d @widget.jenkins-job-notbuilt.json $ENDPOINT --header "Content-Type:application/json"
+curl -X POST -d @widget.jenkins-job-disabled.json $ENDPOINT --header "Content-Type:application/json"
+curl -X POST -d @widget.jenkins-job-green.json    $ENDPOINT --header "Content-Type:application/json"
+curl -X POST -d @widget.jenkins-job-yellow.json   $ENDPOINT --header "Content-Type:application/json"
+curl -X POST -d @widget.jenkins-job-aborted.json  $ENDPOINT --header "Content-Type:application/json"
+curl -X POST -d @widget.jenkins-job-red.json      $ENDPOINT --header "Content-Type:application/json"
+
