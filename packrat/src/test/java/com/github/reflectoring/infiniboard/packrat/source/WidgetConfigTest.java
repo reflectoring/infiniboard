95c8b0b796bc8fc4b1af1c3ff16b73e01203205c
Matthias Balke <matthias.balke@gmail.com>
2017-01-31 23:34:02 +0100

    added tests for masking credentials in API (#164)
    replaced deprecated annotations

--- packrat/src/test/java/com/github/reflectoring/infiniboard/packrat/source/WidgetConfigTest.java
+++ packrat/src/test/java/com/github/reflectoring/infiniboard/packrat/source/WidgetConfigTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.github.reflectoring.infiniboard.packrat.PackratTestApplication;
@@ -21,7 +21,7 @@ import com.github.reflectoring.infiniboard.test.categories.MongoIntegrationTests
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = PackratTestApplication.class)
+@SpringBootTest(classes = PackratTestApplication.class)
 @Category(MongoIntegrationTests.class)
 public class WidgetConfigTest {
 
--- quartermaster/src/test/java/com/github/reflectoring/infiniboard/quartermaster/QuartermasterTest.java
+++ quartermaster/src/test/java/com/github/reflectoring/infiniboard/quartermaster/QuartermasterTest.java
@@ -3,7 +3,7 @@ package com.github.reflectoring.infiniboard.quartermaster;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -11,7 +11,7 @@ import com.github.reflectoring.Quartermaster;
 import com.github.reflectoring.infiniboard.test.categories.MongoIntegrationTests;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Quartermaster.class)
+@SpringBootTest(classes = Quartermaster.class)
 @WebAppConfiguration
 @Category(MongoIntegrationTests.class)
 public class QuartermasterTest {
