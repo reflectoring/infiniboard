c6de01ec79abb894dd6cef06405ec8f331c281f9
Matthias Balke <matthias.balke@googlemail.com>
2019-05-08 17:57:22 +0200

    create GitHub release (#434)
    * modify release publication script to work with GitLab CI (#415)
    * create GitHub release on deployment (#415)

--- github-promote-release.sh
+++ github-promote-release.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+shopt -s extglob
 #set -x
 
 USERNAME=reflectoring
 REPOSITORY=infiniboard
 
-if [ $# -ne 2 ];
+if [ $# -ne 1 ];
 then
-  echo "usage: github-promote-release.sh <CIRCLE_CI_BUILD_NUMBER> <RELEASE_VERSION>"
+  echo "usage: github-promote-release.sh <RELEASE_VERSION>"
   exit 1;
 fi
 
-BUILD_NUMBER=$1
-TAG_VERSION=$2
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Environment variable GITHUB_TOKEN must be set. Aborting!"
+    exit 2;
+fi
+
+TAG_VERSION=$1
 
 command -v jq >/dev/null 2>&1 || { echo >&2 "\"jq\" is required but it's not installed.  Aborting."; exit 1; }
 command -v github-release >/dev/null 2>&1 || {
@@ -46,41 +51,9 @@ command -v github-release >/dev/null 2>&1 || {
   }
 }
 
-CIRCLE_BUILD_URL=https://circleci.com/api/v1.1/project/github/$USERNAME/$REPOSITORY/$BUILD_NUMBER
-CIRCLE_ARTIFACTS_URL=$CIRCLE_BUILD_URL/artifacts
-
-# get VCS revision
-echo "fetching git hash for build #$BUILD_NUMBER"
-JSON=$(curl -s $CIRCLE_BUILD_URL)
-COMMIT_HASH=$(echo $JSON | jq -r '.vcs_revision')
-echo "> $COMMIT_HASH"
-echo ""
-
-echo "fetching artifacts for build #$BUILD_NUMBER"
-JSON_ARTIFACTS=$(curl -s $CIRCLE_ARTIFACTS_URL)
-
-# create download folder
-DOWNLOAD_DIR=`pwd`/build/download
-rm -rf $DOWNLOAD_DIR
-mkdir -p $DOWNLOAD_DIR
-cd $DOWNLOAD_DIR
-
-# download matching artifacts
-echo "downloading artifacts ..."
-# TODO extract download file function
-echo $JSON_ARTIFACTS | jq '.[].url' | while read artifact; do
-    if [[ $artifact == *"war"* ]]
-    then
-        ARTIFACT=$(echo $artifact | cut -d '"' -f 2)
-        echo "> $ARTIFACT"
-        curl -s -L -O $(echo $ARTIFACT)
-    fi
-done
-echo ""
-
 echo "creating git tag '$TAG_VERSION' ..."
 # create git tag
-git tag -a $TAG_VERSION $COMMIT_HASH -m "released version $TAG_VERSION"
+git tag -a $TAG_VERSION $CI_COMMIT_SHA -m "released version $TAG_VERSION"
 echo ""
 
 echo "pushing git tag $TAG_VERSION ..."
@@ -99,7 +72,7 @@ github-release release \
 echo ""
 
 echo "uploading github release artifacts ..."
-for artifact in $DOWNLOAD_DIR/*.*; do
+for artifact in {quartermaster/build/libs/quartermaster*.war,harvester/build/libs/harvester*.war}; do
     echo "> $(basename $artifact)"
 
     # upload artifacts
